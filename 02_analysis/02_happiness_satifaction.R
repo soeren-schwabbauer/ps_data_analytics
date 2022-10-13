50d3c77bd2214ecae3d3c0bdeffbb9ee2fe09c49
soeren-schwabbauer <soeren.schwabbauer@uibk.ac.at>
2022-10-13 23:52:06 +0200

    new folder for analysis/start analysis for income

---
+++ 02_analysis/02_happiness_satifaction.R
@@ -0,0 +1,66 @@
+rm(list = ls())
+################################################################################
+# Course: Data Analytics - PS
+# Date: 13.10.2022
+# Purpose: 1) explore main variables
+#          2) compare main variables to other variables
+################################################################################
+
+# load libraries
+library(dplyr)
+library(haven)
+library(ggplot2)
+library(ggmosaic)
+
+# load df
+INPUT = "G:/Geteilte Ablagen/data_analytics/02_analysis/INPUT/"
+OUTPUT = "G:/Geteilte Ablagen/data_analytics/02_analysis/OUTPUT/"
+#CODEBOOK = "G:/Geteilte Ablagen/data_analytics/"
+
+
+##### load files
+# load dataframe
+load(paste0(INPUT, "Romania.rda"))
+
+###### explore main variables ##################################################
+
+### A170 satisfaction in life
+romania$satisfaction_life
+summary(romania$satisfaction_life)
+# scaled from 1 to ten
+
+
+### A008 Feeling of happiness
+romania$feel_happy
+as_factor(feel_happy)
+summary(romania$feel_happy)
+# scaled from 1 to 4, 1 is best, 4 is worst
+
+
+###### compare main variables to  income, health, age, marital status ##########
+# mosaic tutorial: https://haleyjeppson.github.io/ggmosaic/reference/geom_mosaic.html
+
+### income (Sören) ###########
+# comparing two quantitative variables by mosaic plot
+romania$feel_happy <- as_factor(romania$feel_happy)
+romania$income_scale <- as_factor(romania$income_scale)
+
+ggplot(data = romania) +
+  geom_mosaic(aes(x =  product(feel_happy, income_scale), fill = feel_happy))
+
+# note: it can be seen, that ...
+
+
+
+### health ###########
+
+
+
+### age ###########
+
+
+
+### marital status ###########
+
+
+### additional variables ###########
