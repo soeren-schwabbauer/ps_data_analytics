ede31a90994ecfaeab5512d249729651545fc4b3
soeren-schwabbauer <soeren.schwabbauer@uibk.ac.at>
2022-11-01 17:30:07 +0100

    first attempt

--- 02_analysis/04_pca.R
+++ 02_analysis/04_pca.R
@@ -14,6 +14,8 @@ library(gridExtra)
 library(webr)
 library(patchwork)
 library(reshape2)
+library(factoextra)
+
 
 
 # load df
@@ -34,3 +36,16 @@ if (dir.exists("G:/Geteilte Ablagen/")) {
 
 # load dataframe
 load(paste0(INPUT, "Romania.rda"))
+
+
+#possible variables for political interest:
+pc.politics <- romania %>% 
+  select(intrests_politics, politics_satisfaction, politics_democracy, importance_democracy, politics_petition,
+         politics_boycott,
+         politics_demo,
+         politics_strikes,
+  ) %>%
+  
+  prcomp( scale = TRUE)
+
+fviz_eig(pc.politics, addlabels = TRUE)
