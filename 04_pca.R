8a46c985817db1e3126b2a9be3395e933b3dbdc4
soeren-schwabbauer <soeren.schwabbauer@uibk.ac.at>
2022-11-05 14:56:02 +0100

    pca to markdown

--- 04_pca.R
+++ 04_pca.R
@@ -15,7 +15,7 @@ library(webr)
 library(patchwork)
 library(reshape2)
 library(factoextra)
-
+library(spatstat.utils)
 
 
 # load df
@@ -32,17 +32,71 @@ if (dir.exists("G:/Geteilte Ablagen/")) {
 # load dataframe
 load(paste0(INPUT, "Romania.rda"))
 
+# The key variable for our analysis is going to be the E023 - Interest in politics (interest_politics) variable. However, the survey offers more questions in regards to the attitude towards politics. The variables in the following graph are going to be key elements in the following principal components analysis.
 
-#possible variables for political interest:
-pc.politics <- romania %>% 
-  select(intrests_politics, politics_satisfaction, politics_democracy, importance_democracy, politics_petition,
+### select relevant variables
+politics_variables <- romania %>% 
+  select(intrests_politics, politics_petition, 
          politics_boycott,
          politics_demo,
          politics_strikes,
-         politics_
-  ) %>%
+         
+         politics_satisfaction, 
+         politics_democracy,
+         importance_democracy, 
+  ) 
+politics_variables <- politics_variables %>% rownames_to_column()
+
+politics_var <- function(varchar) {
+  politics_variables %>%
+  mutate_all(function(x) droplevels(as_factor(x))) %>% 
+  melt(id.vars = "rowname", variable.name = "factor", value.name = "level") %>%
   
-  prcomp( scale = TRUE)
+  group_by(factor, level) %>%
+  summarise(n = n()) %>%
+  mutate(freq = n / sum(n)) %>%
+  mutate(freq_cum = revcumsum(freq)) %>%
+  filter(factor == {{varchar}}) %>%
+  
+  ggplot(aes(y = freq, x = factor, fill = level)) +
+    geom_bar(postition = "dodge", stat = "identity") +
+
+geom_text(aes(y = freq_cum, label = level), vjust = -1, colour = "white") +
+scale_y_continuous(labels = scales::percent) +
+
+theme(
+  axis.title.x = element_blank(),
+  axis.title.y = element_blank(),
+  panel.background = element_rect(fill = "white"),
+  panel.grid.major.y = element_line(size = 0.5, linetype = 'solid',
+                                    colour = "grey"),
+  plot.title = element_text(color="black", size= 12, face="bold.italic", vjust = 0.1),
+  legend.position = "none"
+)
+}
+
+grid.arrange(politics_var("intrests_politics" ), politics_var("politics_petition"), politics_var("politics_boycott"), politics_var("politics_demo"), ncol = 4)
+grid.arrange(politics_var("politics_strikes"), politics_var("politics_satisfaction"), politics_var("politics_democracy"), politics_var("importance_democracy"), ncol = 4)
+
+# In order to avoid misunderstnding, we want to give a quick overview of what the variables mean. 
+
+#  | **id**  | **variable**          | **explaination**                                  |
+#  |---------|-----------------------|---------------------------------------------------|
+#  | E023    | intrests_politics     | How interested would you say you are in politics? |
+#  | E025    | politics_petition     | Signing a petition                                |
+#  | E026    | politics_boycott      | Joining in boycotts                               |
+#  | E027    | politics_demo         | Attending lawful demonstrations                   |
+# | E028    | politics_strikes      | Joining unofficial strikes                        |
+#  | E111_01 | politics_satisfaction | Satisfaction with the political system            |
+#  | E117    | politics_democracy    | Having a democratic political system              |
+#  | E235    | importance_democracy  | Importance of democracy                           |
+
+
+
+
+
+  
+pc.politics <-  politics_variables %>% prcomp( scale = TRUE)
 
 fviz_eig(pc.politics, addlabels = TRUE)
 
