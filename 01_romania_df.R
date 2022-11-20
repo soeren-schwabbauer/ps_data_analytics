2174f3f1e0e9d8f424f2a89480ba369a33ea35fa
soeren-schwabbauer <soeren.schwabbauer@uibk.ac.at>
2022-11-20 14:11:41 +0100

    adjust scaling

--- 01_romania_df.R
+++ 01_romania_df.R
@@ -9,6 +9,7 @@ rm(list = ls())
 library(dplyr)
 library(readxl)
 library(haven)
+library(codebook)
 
 # load df
 if (dir.exists("G:/Geteilte Ablagen/")) {
@@ -46,6 +47,12 @@ romania <- data %>%
   
   rename_at(vars(paste0(var_id, "")), function(x) variable)  # rename variables
 # A003 doesn't exist in df
+
+
+
+### flip certain variables
+# higher number -> better
+# lower number -> worse
 
 
 
@@ -102,6 +109,59 @@ romania$marst_fac <- droplevels(romania$marst_fac)
 romania$health_fac <- droplevels(romania$health_fac)
 romania$sex <-  droplevels(romania$sex_fac)
 romania$trust_fac <- droplevels(romania$trust_fac)
+
+
+
+# check for all variables
+
+# okay for:
+# satisfaction 
+reverse <- c("intrests_politics")
+reverse_fac <- paste0(reverse, "_fac")
+
+reverse <- function(var, var_fac){
+  romania$var_fac <- as_factor(romania$var)
+  romania$var_fac <- droplevels(romania$var_fac)
+  romania$var <- reverse_labelled_values(romania$var_fac)
+
+}
+
+reverse(intrests_politics, intrests_politics_fac)
+
+reverse_labelled_values(happy_fac)
+reverse_labelled_values(health_fac)
+reverse_labelled_values(trust_fac)
+
+romania$intrests_politics_fac <- as_factor(romania$intrests_politics)
+romania$intrests_politics_fac <- droplevels(romania$intrests_politics_fac)
+romania$intrests_politics <- reverse_labelled_values(romania$intrests_politics_fac)
+
+romania$politics_petition_fac <- as_factor(romania$politics_petition)
+romania$politics_petition_fac <- droplevels(romania$politics_petition_fac)
+romania$politics_petition <- reverse_labelled_values(romania$politics_petition_fac)
+
+romania$politics_boycott_fac <- as_factor(romania$politics_boycott)
+romania$politics_boycott_fac <- droplevels(romania$politics_boycott_fac)
+romania$politics_boycott <- reverse_labelled_values(romania$politics_boycott_fac)
+
+
+romania$politics_demo_fac <- as_factor(romania$politics_demo)
+romania$politics_demo_fac <- droplevels(romania$politics_demo_fac)
+romania$politics_demo <- reverse_labelled_values(romania$politics_demo_fac)
+
+romania$politics_demo_fac <- as_factor(romania$politics_demo)
+romania$politics_demo_fac <- droplevels(romania$politics_demo_fac)
+romania$politics_demo <- reverse_labelled_values(romania$politics_demo_fac)
+attach(romania)
+
+intrests_politics
+politics_petition
+politics_boycott
+politics_demo
+politics_strikes
+politics_satisfaction
+politics_democracy
+importance_democracy 
 
 ##### save file
 save(romania, file = paste0(OUTPUT, "romania.rda"))
