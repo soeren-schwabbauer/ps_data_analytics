55fb639b8b7c51af9f5a9f81952bd8d89196f6b9
ShizzleMaDizzle <david.hollenstein@student.uibk.ac.at>
2022-12-05 10:10:51 +0100

    Finish Task 7

--- 07_freedom.R
+++ 07_freedom.R
@@ -35,12 +35,12 @@ source("99_functions.R")
 
 
 
-# An additional variable, that can influence life satisfaction and happiness in life is "How much freedom of choice and control" somebody has. The authors of the study describe the question as follows: ome people feel they have completely free choice and control over their lives, and other people feel that what they do has no real effect on what happens to them. Please use the scale to indicate how much freedom of choice and control you feel you have over the way your life turns out?
+# An additional variable, that can influence life satisfaction and happiness in life is "How much freedom of choice and control" somebody has. The authors of the study describe the question as follows: some people feel they have completely free choice and control over their lives, and other people feel that what they do has no real effect on what happens to them. Please use the scale to indicate how much freedom of choice and control you feel you have over the way your life turns out?
 
 # In order to get a first overview, let us have a look at the distribution of the variable.
 bar_freedom <- barplot_var(freedom_fac)
 box_freedom <- boxplot_for_barplot(freedom) +
-  labs(title = "How much freedom of choice and control") 
+  labs(title = "How much freedom of choice and control do you think you have?") 
 grid.arrange(box_freedom, bar_freedom, nrow = 2, heights = c(0.5,2))
 # From looking at the boxplot we can see, that the median of the distribuiton is at the level of 8. Most people (32%) see themself to be very free, by answering at the highest level possible. almost 50% of the distribution chose a level the level of 9. The additional 50% of the remaining answers are distibuted in a declining distibution from 8 to 1. The lower levels, especially 1 to 4) may not have a lot of observations, however we do have quiet some oversvations for the upper levels. We therefor decided to bundle the variable as the follows: 
 
@@ -55,7 +55,13 @@ grid.arrange(box_freedom, bar_freedom, nrow = 2, heights = c(0.5,2))
 summary(as_factor(romania$freedom_group))
 barplot_var(freedom_group) +
   labs(title = "How much freedom of choice and control - by groups")
-# Let us now look if we can find some associations between lifesatisfaction or happiness in life together with the freedom in life. 
+
+# Looking at this distribution, we can observe that most of the respondents seem to feel at least a little in control of how their lives turn out. This is something we need to keep in mind when looking at the correlations between this feeling of control and our dependent variables.
+
+
+# Let us now look if we can find some associations between life satisfaction or happiness in life together with the perceived control that citizens feel they have in their lives. 
+
+
 freedom_happy <-  romania %>%
   ggplot() +
   geom_mosaic(aes(x =  product(happy_fac, freedom_group), fill = happy_fac)) +
@@ -74,22 +80,7 @@ freedom_satis <- romania %>%
 
 grid.arrange(freedom_happy, freedom_satis, nrow = 1)
 
-# From looking at the stacked barplot showing the realtion between happiness and freedom of choice and control we can
-# see that: 
-# - a very large portion of persons rate their happiness as "very happy" and "quite happy" group 3&4 more than 1&2
-# - looking at the "very happy" persons we can see that there is a trend that more people are "very happy" the
-# more control and freedom they possess in their lives, except for the persons in group 1 "not much" freedom are
-# almost equally "very happy" as grouß 3 which "rather agrees" with freedom of choice and control. Looking at the
-# "quite happy" portion of the graph there is no real trend as grouß 2&3 have more "quite happy" mentions than for 
-# for example group 4 where it rather would be expected. 
-# In the "not very happy" and "not at all happy" portions we cannot see a clear trend.
+# From looking at the mosaic plot, showing the relation between perceived freedom and happiness, a trend that we can observe is that the more people feel in control of their lives, the happier they tend to be. As previously mentioned we need to keep in mind that for those who feel not so much in control of their lives (group 1) we have a rather small sample size. This could be the reason why the trend that we can observe in groups 2 and 3 is not so much reflected in group 1. Looking at group 4 in particular, the trend also seems to be interrupted. However we can observe a higher percentage of respondents who feel like they have a lot of influence on their lives who also claim to be very happy as compared to groups 2 or 3.
 
-# Analyzing the right graph which compares satisfaction in life with freedom of choice and control we notice:
-# here a much clearer trend across the board, we could generally say that the more freedom of choice and control
-# where perceived the more satisfied the persons where. The small irregularities in the very satisfied portion
-# where group 1 " not much" freedom was more "very satisfied" or in the portion "not satisfied at all" in group 1 &3
-# could be explained by samplesize, as the median was mentioned to be 8 so not a lot of sample size at least in 
-# "not much" and "a little" freedom of choice and control. 
+# Analyzing the right graph which compares satisfaction in life to freedom of choice and control, we notice a much clearer trend across the board. We could generally say that the more the Romanian people feel in control of their lives, the more satisfied they were. It needs to be noted, that especially in the group that feels not so much in control (group 1) we are working with a rather small sample size. However, this does not diminish the trend otherwise observed in the dataset and leads us to the conclusion that we will consider this variable further.
 
-# We should further do correlation test and significance tests to show if trends are significant or not due to 
-# sample size or variance
